6922aa34be31813286f74d38816fb4df2f0664f1
etran12 <XoreandoX@hotmail.com>
2014-01-22 10:57:09 -0500

    Lab 1
    Lab 1 for BTI420

---
+++ lab1/lab1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person person { get; set; }
+        private Transcript transcript { get; set; }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Transcript Page.";
+            person = new Person(2, "Tran", "Elvis", 22, "");
+            transcript = new Transcript(person);
+            transcript.Grades.Add(new Grade("BTD310", "A"));
+            transcript.Grades.Add(new Grade("BTP400", "A"));
+            transcript.Grades.Add(new Grade("BTI210", "C"));
+            return View(transcript);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            person = new Person(1, "Tran", "Elvis", 22, "Content/images/headshot.jpg");
+            return View(person);
+        }
+    }
+}
---
+++ lab1/lab1/Controllers/Home_vm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1.Controllers
+{
+    public class Person
+    {
+        public int Id;
+        public string LastName;
+        public string GivenNames;
+        public int Age;
+        public string PhotoUrl;
+
+        public Person(int id, string lastName, string givenNames, int age, string photoURL)
+        {
+            Id = id;
+            LastName = lastName;
+            GivenNames = givenNames;
+            Age = age;
+            PhotoUrl = photoURL;
+        }
+
+        public string toString()
+        {
+            return String.Format("{2} {1} ID - {0}, is {3} years old", Id, LastName, GivenNames, Age); 
+        }
+    }
+
+    public class Grade
+    {
+        public string Code;
+        public string LetterGrade;
+
+        public Grade(string code, string letterGrade)
+        {
+            Code = code;
+            LetterGrade = letterGrade;
+        }
+
+        public string toString()
+        {
+            return String.Format("Course Code: {0}        Grade: {1} \n", Code, LetterGrade);
+        }
+    }
+
+    public class Transcript
+    {
+        public Person student;
+        public ICollection<Grade> Grades;
+        public double GPA;
+        public Transcript(Person person)
+        {
+            student = person;
+            this.Grades = new List<Grade>();
+            GPA = 0;
+        }
+    }
+}
